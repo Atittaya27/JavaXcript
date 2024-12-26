bed2f05240507cf220649192684c75e36bdaab1c
Atittaya <maiatittaya.a@gmail.com>
2024-12-26 11:06:36 +0700

    first commit

---
+++ Lecture_3/ArrayPractice.js
@@ -0,0 +1,7 @@
+let furniture = ['table' , 'Chairs','Couch'];
+
+for (let name of furniture){
+    console.log(name);
+    for (let char of name)
+        console.log(char);
+    }
--- Lecture_3/mapReformatArray.js
+++ Lecture_3/mapReformatArray.js
@@ -5,7 +5,7 @@ const kvArray = [
 ];
 
 const reformattedArray = kvArray.map(({key,value}) =>
-({[key]: value}));
+({ [key]: value}));
 
 console.log(reformattedArray);
 console.log(kvArray);
